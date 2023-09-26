8b8d671b6c7b30a0de6583fa207befe0f8d9687a
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-26 12:55:16 -0400

    code

--- main.ts
+++ main.ts
@@ -5,4 +5,21 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+
+// variables
+let temperature: number
+
+input.onButtonPressed(Button.A, function () {
+  // get temperature value
+  temperature = input.temperature()
+
+  // process
+  temperature = temperature + 273.15
+  temperature = Math.round(temperature)
+
+  //output
+  basic.showString('The temperature is ' + temperature.toString() + ' K')
+})
