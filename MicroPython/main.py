edfe93d845e5a7dffd5be0e84fb69e2b2529e2c5
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-09-26 17:14:26 +0000

    python vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Sophie
+Created on: Sep 2023
+This module is a Micro:bit MicroPython program and shows temperature in kelvin
 """
 
 from microbit import *
 
-
 display.scroll("Hello, World!")
+display.clear
+display.show(Image.HEART)
+
+temperature = 0
+
+while True:
+    if button_a.is_pressed():
+        temperature = temperature()
+        temperature = temperature + 273.15
+        display.scroll("The temperature is " + str(temperature) + " K")
